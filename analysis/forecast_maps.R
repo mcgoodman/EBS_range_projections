df9e23c1127090acfb7f984da3bafcac3715893b
Maurice Codespoti Goodman <goodman.maurice@gmail.com>
2025-05-04 10:30:10 -0500

    biomass forecast maps

--- analysis/forecast_maps.R
+++ analysis/forecast_maps.R
@@ -25,7 +25,9 @@ summarize_proj <- function(x, start = 1995, end = 2014, var = p_occurrence, f = 
   
 }
 
-# Read in hindcasts and forecasts, compute difference ---------------
+# Probability of occurrence forecasts -------------------------------
+
+## Read in hindcasts and forecasts, compute difference --------------
 
 sp_dirs <- list.files(here("output"), full.names = TRUE)
 sp_dirs <- sp_dirs[sp_dirs != save_dir]
@@ -41,14 +43,12 @@ for (i in seq_along(sp_dirs)) {
   # Read in forecast
   forecasts[[i]] <- readRDS(file.path(sp_dirs[i], "forecast_level2.rds"))
   
-  # Compute anomaly and anomaly Z-score
+  # Compute anomaly
   anomalies[[i]] <- forecasts[[i]] |> 
     mutate(p_occurrence_diff = pmin(pmax(p_occurrence - c(hindcasts[[i]]$p_occurrence), -0.33), 0.33)) |> 
     select(p_occurrence_diff)
   
 }
-
-# Plot --------------------------------------------------------------
 
 # Read in Alaska coast, transform maps to UTM for plotting
 ak_coast <- get_ak_coast()
@@ -57,6 +57,8 @@ forecasts <- lapply(forecasts, st_transform, crs = st_crs(ak_coast))
 anomalies <- lapply(anomalies, st_transform, crs = st_crs(ak_coast))
 ak_coast <- filter(st_crop(ak_coast, hindcasts$`walleye_pollock-adult`), is.na(DESC_))
 ebs <- get_ebs_shapefile()
+
+## Plot -------------------------------------------------------------
 
 for (i in seq_along(sp_dirs)) {
   
@@ -109,3 +111,133 @@ for (i in seq_along(sp_dirs)) {
   ggsave(file.path(sp_dirs[i], "occurrence_forecast.png"), joined, height = 4, width = 10, units = "in", dpi = 300)
   
 }
+
+# Relative biomass forecasts ----------------------------------------
+
+## Read in hindcasts and forecasts, compute difference --------------
+
+hindcasts <- forecasts <- anomalies <- setNames(vector("list", length(sp_dirs)), basename(sp_dirs))
+
+for (i in seq_along(sp_dirs)) {
+  
+  # Read in hindcast, compute average probability of occurrence for reference period
+  hindcasts[[i]] <- readRDS(file.path(sp_dirs[i], "hindcast_level2.rds"))
+  hindcasts[[i]] <- c(
+    summarize_proj(mutate(hindcasts[[i]], log_biomass = log(biomass_fit)), var = log_biomass), 
+    summarize_proj(hindcasts[[i]], var = biomass_fit)
+  )
+  
+  # Read in forecast
+  forecasts[[i]] <- mutate(readRDS(file.path(sp_dirs[i], "forecast_level2.rds")), log_biomass = log(biomass_fit))
+  
+  # Center both, to compute relative biomass distribution anomaly
+  hindcasts[[i]]$log_biomass_ctr <- hindcasts[[i]]$log_biomass - mean(hindcasts[[i]]$log_biomass, na.rm = TRUE)
+  forecasts[[i]]$log_biomass_ctr <- forecasts[[i]]$log_biomass - mean(forecasts[[i]]$log_biomass, na.rm = TRUE)
+  
+  # Biomass proportion
+  hindcasts[[i]]$biomass_prop <- hindcasts[[i]]$biomass_fit/sum(hindcasts[[i]]$biomass_fit, na.rm = TRUE)
+  forecasts[[i]]$biomass_prop <- forecasts[[i]]$biomass_fit/sum(forecasts[[i]]$biomass_fit, na.rm = TRUE)
+  
+  # Compute anomaly and anomaly Z-score
+  anomalies[[i]] <- forecasts[[i]] |> mutate(
+    #log_biomass_diff = pmax(pmin(log_biomass_ctr - c(hindcasts[[i]]$log_biomass_ctr), 3), -3),
+    log_biomass_diff = log_biomass_ctr - c(hindcasts[[i]]$log_biomass_ctr),
+    log_biomass_zscore = pmax(pmin(log_biomass_diff / biomass_se, 3), -3), 
+    biomass_prop_diff = biomass_prop - c(hindcasts[[i]]$biomass_prop)
+  )
+  
+}
+
+# Transform maps to UTM for plotting
+hindcasts <- lapply(hindcasts, st_transform, crs = st_crs(ak_coast))
+forecasts <- lapply(forecasts, st_transform, crs = st_crs(ak_coast))
+anomalies <- lapply(anomalies, st_transform, crs = st_crs(ak_coast))
+
+## Plot -------------------------------------------------------------
+
+for (i in seq_along(sp_dirs)) {
+  
+  plot_range <- c(0, max(c(hindcasts[[i]]$biomass_prop), c(forecasts[[i]]$biomass_prop)))
+  
+  hindcast_plot <- ggplot() + 
+    geom_stars(aes(fill = biomass_prop, color = biomass_prop), data = hindcasts[[i]]) +
+    geom_sf(data = ak_coast, fill = "grey80", color = NA) + 
+    geom_sf(data = ebs, fill = NA, color = "black", linewidth = 0.5) +
+    scale_color_viridis_c(option = "magma", na.value = NA, limits = plot_range) + 
+    scale_fill_viridis_c(option = "magma", na.value = NA, limits = plot_range) + 
+    coord_sf(expand = FALSE) + 
+    theme_void() + 
+    theme(legend.position = "top", plot.background = element_rect(fill = "white", color = NA)) + 
+    guides(
+      color = guide_colorbar("1995-2014 biomass proportion", title.position = "top", title.hjust = 0.5, barwidth = unit(12, "lines"), ticks.colour = "black", frame.colour = "black"), 
+      fill = guide_colorbar("1995-2014 biomass proportion", title.position = "top", title.hjust = 0.5, barwidth = unit(12, "lines"), ticks.colour = "black", frame.colour = "black")
+    )
+  
+  forecast_plot <- ggplot() + 
+    geom_stars(aes(fill = biomass_prop, color = biomass_prop), data = forecasts[[i]]) +
+    geom_sf(data = ak_coast, fill = "grey80", color = NA) + 
+    geom_sf(data = ebs, fill = NA, color = "black", linewidth = 0.5) +
+    scale_color_viridis_c(option = "magma", na.value = NA, limits = plot_range) + 
+    scale_fill_viridis_c(option = "magma", na.value = NA, limits = plot_range) + 
+    coord_sf(expand = FALSE) + 
+    theme_void() + 
+    theme(legend.position = "top", plot.background = element_rect(fill = "white", color = NA)) + 
+    guides(
+      color = guide_colorbar("Forecast biomass proportion", title.position = "top", title.hjust = 0.5, barwidth = unit(12, "lines"), ticks.colour = "black", frame.colour = "black"), 
+      fill = guide_colorbar("Forecast biomass proportion", title.position = "top", title.hjust = 0.5, barwidth = unit(12, "lines"), ticks.colour = "black", frame.colour = "black")
+    )
+  
+  anomaly_plot <- ggplot() + 
+    geom_stars(aes(fill = biomass_prop_diff, color = biomass_prop_diff), data = anomalies[[i]]) +
+    geom_sf(data = ak_coast, fill = "grey80", color = NA) + 
+    geom_sf(data = ebs, fill = NA, color = "black", linewidth = 0.5) +
+    scale_color_gradient2(low = "#08306B", high = "#67000D", na.value = NA) + 
+    scale_fill_gradient2(low = "#08306B", high = "#67000D", na.value = NA) + 
+    scale_alpha_continuous(range = c(0, 1)) + 
+    coord_sf(expand = FALSE) + 
+    theme_void() + 
+    theme(legend.position = "top", plot.background = element_rect(fill = "white", color = NA)) + 
+    guides(
+      color = guide_colorbar(expression(Delta~"proportion biomass"), title.position = "top", title.hjust = 0.5, barwidth = unit(12, "lines"), ticks.colour = "black", frame.colour = "black"), 
+      fill = guide_colorbar(expression(Delta~"proportion biomass"), title.position = "top", title.hjust = 0.5, barwidth = unit(12, "lines"), ticks.colour = "black", frame.colour = "black")
+    )
+  
+  joined <- cowplot::plot_grid(
+    hindcast_plot, forecast_plot, anomaly_plot, nrow = 1
+  ) + theme(plot.background = element_rect(fill = "white", color = NA))
+  
+  ggsave(file.path(sp_dirs[i], "biomass_forecast.png"), joined, height = 4, width = 10, units = "in", dpi = 300)
+  
+}
+
+# MOM6 / ROMS anomaly -----------------------------------------------
+
+roms_hindcast <- readRDS(here("data", "roms_level2_bc_annual", "CORECFS_hindcast.rds"))
+mom6_forecast <- readRDS(here("data", "mom6", "mom6_forecast.rds"))
+
+roms_hindcast <- c(
+  summarize_proj(roms_hindcast, var = temp_bottom5m), 
+  summarize_proj(roms_hindcast, var = oxygen_bottom5m), 
+  summarize_proj(roms_hindcast, var = pH_bottom5m)
+)
+
+mom6_anomaly <- mom6_forecast |> mutate(
+  temp_diff = temp_bottom5m - c(roms_hindcast$temp_bottom5m), 
+  oxygen_diff = oxygen_bottom5m - c(roms_hindcast$oxygen_bottom5m), 
+  pH_diff = pH_bottom5m - c(roms_hindcast$pH_bottom5m)
+)
+
+ggplot() + 
+  geom_stars(aes(fill = temp_diff, color = temp_diff), data = mom6_anomaly) + 
+  scale_color_gradient2(low = "#08306B", high = "#67000D", na.value = NA) + 
+  scale_fill_gradient2(low = "#08306B", high = "#67000D", na.value = NA)
+
+ggplot() + 
+  geom_stars(aes(fill = oxygen_diff, color = oxygen_diff), data = mom6_anomaly) + 
+  scale_color_gradient2(low = "#08306B", high = "#67000D", na.value = NA) + 
+  scale_fill_gradient2(low = "#08306B", high = "#67000D", na.value = NA)
+
+ggplot() + 
+  geom_stars(aes(fill = pH_diff, color = pH_diff), data = mom6_anomaly) + 
+  scale_color_gradient2(low = "#08306B", high = "#67000D", na.value = NA) + 
+  scale_fill_gradient2(low = "#08306B", high = "#67000D", na.value = NA)
